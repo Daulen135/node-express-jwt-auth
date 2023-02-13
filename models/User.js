8c6ffe055aee45db8fa95de9566fbda1a9449a39
daulen <daulenz@gmail.com>
2023-02-13 16:45:12 -0600

    User login and JWT

--- models/User.js
+++ models/User.js
@@ -1,46 +1,42 @@
-const mongoose = require('mongoose')
-const {isEmail} = require('validator')
-const bcrypt = require('bcrypt')
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
-const userSchema  =new mongoose.Schema({
-
-    email: {
-        type: String,
-        required: [true, "Please enter e-mail"],
-        unique: true,
-        lowercase: true,
-        validate: [isEmail, "Please enter valid email"]
-    },
-
-    password: {
-        type: String,
-        required: [true, "please add password"], 
-        minlength: [6, "Min password length is 6 characters"]
-    },
-})
-
-//fire a fucntion after doc saved (Mongoose hook)
-
-userSchema.post('save', function(doc, next){
-    console.log("new user was created and saved", doc);
-    next();
-})
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  }
+});
 
 
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-//fire a fucntion before doc saved (Mongoose hook)
-
-userSchema.pre('save', async function(next){
-    const salt  = await bcrypt.genSalt();//adding salt
-    // console.log("user about to be created and saved", this);
-    this.password = await bcrypt.hash(this.password, salt)
-    //hashed password goes to DB
-    next();
-})
-
-
-
-
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
 
 const User = mongoose.model('user', userSchema);
 
